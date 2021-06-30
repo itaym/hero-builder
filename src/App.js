ea1f5f2d917edc442349202f9bf6ce73b6ff3f38
itaym <itaymer@gmail.com>
2021-07-01 02:21:19 +0300

    Playing with empty place holder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import styles from './App.module.scss';
-import HeroCard from "./components/HeroCard";
-import VerticalNav from "./components/VerticalNav";
+import HeroCard from "./components/HeroCard"
+import VerticalNav from "./components/VerticalNav"
+import styles from './App.module.scss'
+import { useState } from "react"
+
+const initialState = {
+    heroes: [
+        { id: 0, name: 'Itay', skills: [], image: 'https://frontend-interview-hero-63u64o32qq-uk.a.run.app/portraits/orc.png'},
+        { id: 1, name: '', skills: [], image: ''},
+        { id: 2, name: '', skills: [], image: ''}]
+}
+
 function App() {
-  return (
-    <div className={styles.globalApp}>
-      <div className={styles.globalContainer}>
-          <div>
-              <VerticalNav />
-          </div>
-          <div className={"pageTitle"}>
-              <h1>Create Your Hero!</h1>
-          </div>
-          <div className={styles.heroCardsHolder}>
-              <sapn className={styles.heroCardHolder}><HeroCard /></sapn>
-              <sapn className={styles.heroCardHolder}><HeroCard /></sapn>
-              <sapn className={styles.heroCardHolder}><HeroCard /></sapn>
-          </div>
-      </div>
-    </div>
-  );
+    const [state, setState] = useState(initialState)
+    return (
+        <div className={styles.globalApp}>
+            <div className={styles.globalContainer}>
+                <div>
+                    <VerticalNav />
+                </div>
+                <div className={"pageTitle"}>
+                    <h1>Create Your Hero!</h1>
+                </div>
+                <div className={styles.heroCardsHolder}>
+                    {state.heroes.map((hero)=>{
+                        return (
+                        <div className={styles.heroCardHolder}>
+                            <HeroCard
+                                key={`hero_id_${hero.id}`}
+                                hero={hero} />
+                        </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/HeroCard/HeroCard.js
+++ src/components/HeroCard/HeroCard.js
@@ -2,17 +2,28 @@ import styles from "./HeroCard.module.scss";
 import * as propTypes from "prop-types";
 import React from "react";
 
-const HeroCard = (props) => {
-
+const HeroCard = ({ hero }) => {
+    const noAvatar = '/no_avatar.png'
     return (
         <div
             className={styles.HeroCard}>
             <span
-                className={styles.heroName}>Hello world</span>
+                className={styles.heroName}>{hero.name || <div className={styles.emptyName} />}</span>
+            <img
+                className={styles.heroImage} alt={hero.name} src={hero.image || noAvatar} />
         </div>
     )
 }
 HeroCard.propTypes = {
-    className: propTypes.string,
+    hero: propTypes.objectOf({
+        id: propTypes.Number,
+        name: propTypes.String,
+        image: propTypes.String,
+        skills: propTypes.arrayOf({
+            name: propTypes.String,
+            value: propTypes.Number
+        })
+
+    }),
 }
 export default React.memo(HeroCard)
